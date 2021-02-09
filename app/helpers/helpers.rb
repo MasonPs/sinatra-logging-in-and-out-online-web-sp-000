29fd3b231aafc9e67710cffad545cc29137060b8
Mason Phillips <mjphillips2000@gmail.com>
2021-02-09 02:54:05 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
+require 'sinatra/base'
+
 class Helpers
+  
+  def current_user(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
+  end
+  
+  def is_logged_in?(session[:user_id])
+    !!session[:user_id]
+  end
 end
